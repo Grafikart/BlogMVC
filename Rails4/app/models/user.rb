0340eae486f33322451d4b66929fd41388e81f7a
Cheikh Sidya CAMARA <scicasoft@gmail.com>
2013-09-23 17:19:19 +0000

    crypter password user

--- Rails4/app/models/user.rb
+++ Rails4/app/models/user.rb
@@ -1,5 +1,13 @@
+require 'digest/sha1'
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
   has_many :posts
+  before_create :crypter_pass
+
+  private
+
+    def crypter_pass
+      self.password = Digest::MD5.hexdigest("BlogMVC" + self.password)
+    end
 end
