6925834d59025ffde66facb73147169225bbec24
Anthony <ad06@free.fr>
2013-09-25 16:13:10 +0200

    Ajout de l'app Blog

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'blog'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
