38904af72354925b8c392dbaee903703a8199ec0
Anthony <ad06@free.fr>
2013-09-25 19:06:11 +0200

    Add a Post controller (generic view)

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from blog.models import Post
+from django.views.generic import ListView
+
+class Home(ListView):
+	model               = Post
+	context_object_name = "posts"
+	template_name       = "blog/home.html"
+	paginate_by         = 5
+
+class PostDetails(ListView):
+	model               = Post
+	context_object_name = "posts"
+	template_name       = "blog/post_details.html"
+	paginate_by         = 5
+
+	def get_queryset(self):
+		return Post.objects.filter(post__slug=self.args[0])
