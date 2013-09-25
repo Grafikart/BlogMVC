beef636d1b7dd62c4903f65d75b9d2d2f12a93f0
Anthony <ad06@free.fr>
2013-09-25 19:05:25 +0200

    Add a Post model

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	""" Init model Post """
+	title   = models.CharField('Title', max_length=255, blank=False)
+	content = models.TextField(verbose_name=u'Content')
+	slug    = models.SlugField(blank=False, max_length=255, db_index= True, unique=True)
+	date    = models.DateField(auto_now_add=False, auto_now=False, blank=True, verbose_name='Date')
+
+	class Meta:
+		ordering = ['-date']
