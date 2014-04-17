9cb173a541c5ffd83ea524776e02addd20837535
Fike Etki <etki@etki.name>
2014-04-17 10:03:40 +0400

    Updated tests configurations

--- Yii/tests/_bootstrap.php
+++ Yii/tests/_bootstrap.php
@@ -4,7 +4,6 @@ require_once(__DIR__ . '/_data/YiiBridge/yiit.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii/framework/yiit.php');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 if (!class_exists('Yii', false) || Yii::app() === null) {
-    require_once('../vendor/yiisoft/yii/framework/yiit.php');
     $config = include(__DIR__.'/../config/test.php');
     \Yii::createWebApplication($config);
     \Yii::import('application.tests.classes.base.*');
