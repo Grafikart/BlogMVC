a5b14b62b0c6587f2592423b708f6f8c0abbc25c
Fike Etki <etki@etki.name>
2014-04-17 09:39:46 +0400

    Initial set of pages for testing

---
+++ Yii/tests/_pages/PostPage.php
@@ -0,0 +1,25 @@
+<?php
+
+class PostPage
+{
+    // include url of current page
+    static $URL = '';
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: EditPage::route('/123-post');
+     */
+     public static function route($param)
+     {
+        return static::$URL.$param;
+     }
+
+
+}
