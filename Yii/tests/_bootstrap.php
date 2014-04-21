7e85a5ad67b8e51213d0779e049ab5e41c023c1d
Fike Etki <etki@etki.name>
2014-04-21 09:27:42 +0400

    Fixed invalid autoloader precedence

--- Yii/tests/_bootstrap.php
+++ Yii/tests/_bootstrap.php
@@ -1,8 +1,8 @@
 <?php
-// This is global bootstrap for autoloading 
+// This is global bootstrap for autoloading
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 require_once(__DIR__ . '/_data/YiiBridge/yiit.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii/framework/yiit.php');
-\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 if (!class_exists('Yii', false) || Yii::app() === null) {
     $config = include(__DIR__.'/../config/test.php');
     \Yii::createWebApplication($config);
