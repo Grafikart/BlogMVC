8c7226949bf3e510f295837a0884157bed903fd1
Fike Etki <etki@etki.name>
2014-04-21 06:29:47 +0400

    Initial steps classes

--- Yii/tests/acceptance/_steps/MemberSteps.php
+++ Yii/tests/acceptance/_steps/MemberSteps.php
@@ -1,7 +1,33 @@
 <?php
 namespace WebGuy;
 
+/**
+ * This class represents common action of logged in user.
+ *
+ * @version    Release: 0.1.0
+ * @since      0.1.0
+ * @package    BlogMVC
+ * @subpackage YiiTests
+ * @author     Fike Etki <etki@etki.name>
+ */
 class MemberSteps extends \WebGuy\VisitorSteps
 {
-
+    /**
+     * Publishes a new comment while on post page.
+     *
+     * @param string $text  Comment text.
+     * @param string $email Optional email.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function comment($text, $email=null)
+    {
+        $I = $this;
+        $I->fillField(\PostPage::$commentTextArea, $text);
+        if ($email !== null) {
+            $I->fillField(\PostPage::$commentEmailField, $email);
+        }
+        $I->click(\PostPage::$commentSubmitButton);
+    }
 }
--- Yii/tests/acceptance/_steps/VisitorSteps.php
+++ Yii/tests/acceptance/_steps/VisitorSteps.php
@@ -1,7 +1,53 @@
 <?php
 namespace WebGuy;
 
+/**
+ * This class represents and implements typical visitor actions.
+ *
+ * @version    Release: 0.1.0
+ * @since      0.1.0
+ * @package    BlogMVC
+ * @subpackage YiiTests
+ * @author     Fike Etki <etki@etki.name>
+ */
 class VisitorSteps extends \WebGuy
 {
+    /**
+     * Performs login.
+     *
+     * @param string $login    User login.
+     * @param string $password User password.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function login($login, $password)
+    {
+        $I = $this;
+        $I->amOnPage(\LoginPage::$url);
+        $I->fillField(\LoginPage::$loginField, $login);
+        $I->fillField(\LoginPage::$passwordField, $password);
+        $I->click(\LoginPage::$submit);
+    }
 
+    /**
+     * Leaves a comment from unauthorized user while on post page.
+     *
+     * @param string $comment  Comment text.
+     * @param string $username User's name.
+     * @param string $email    User's email.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function comment($comment, $username, $email=null)
+    {
+        $I = $this;
+        $I->fillField(\PostPage::$commentUsernameField, $username);
+        $I->fillField(\PostPage::$commentTextArea, $comment);
+        if ($email !== null) {
+            $I->fillField(\PostPage::$commentEmailField, $email);
+        }
+        $I->click(\PostPage::$commentSubmitButton);
+    }
 }
