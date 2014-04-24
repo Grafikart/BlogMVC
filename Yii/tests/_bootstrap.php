72fd79528984ea4214b2022d5e298c944612d858
Fike Etki <etki@etki.name>
2014-04-25 01:11:26 +0400

    Updated configuration for unit testing

--- Yii/tests/_bootstrap.php
+++ Yii/tests/_bootstrap.php
@@ -1,10 +1,8 @@
 <?php
 // This is global bootstrap for autoloading
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
-require_once(__DIR__ . '/_data/YiiBridge/yiit.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii/framework/yiit.php');
-if (!class_exists('Yii', false) || Yii::app() === null) {
-    $config = include(__DIR__.'/../config/test.php');
-    \Yii::createWebApplication($config);
-    \Yii::import('application.tests.classes.base.*');
-}
+// this shit sucks
+// Yii isn't found by standard composer autoload
+require_once dirname(__DIR__).'/boot/testing.php';
+require_once __DIR__.'/_helpers/CacheHelper.php';
+\Codeception\Module\CacheHelper::init();
