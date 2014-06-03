5bc7260f4d6646d958ec8620dd451d63b06e8d3c
imikado <imikado@gmail.com>
2014-06-03 20:47:19 +0200

    Utilisation de composer pour installer le framework.

--- mkFramework/module/posts/view/show.php
+++ mkFramework/module/posts/view/show.php
@@ -1,8 +1,8 @@
 <div class="page-header">
 	<h1><?php echo $this->oPosts->name ?></h1>
 	<p><small>
-	Category : <a href="<?php echo _root::getLink('default::category',array('id'=>$this->oPosts->category_id))?>"><?php echo $this->tJoinmodel_categories[$this->oPosts->category_id]?></a>,
-	by <a href="index.html"><?php echo $this->tJoinmodel_users[$this->oPosts->user_id]?></a> on <em><?php $oDate=new plugin_datetime($this->oPosts->created); echo $oDate->toString('D dS Y'); ?></em>
+	Category : <a href="<?php echo _root::getLink('default::category',array('id'=>$this->oPosts->category_id))?>"><?php if(isset($this->tJoinmodel_categories[$this->oPosts->category_id])) echo $this->tJoinmodel_categories[$this->oPosts->category_id]?></a>,
+	by <a href="index.html"><?php if(isset($this->tJoinmodel_users[$this->oPosts->user_id])) echo $this->tJoinmodel_users[$this->oPosts->user_id]?></a> on <em><?php $oDate=new plugin_datetime($this->oPosts->created); echo $oDate->toString('D dS Y'); ?></em>
 	</small></p>
 </div>
 
