c8d192f813c17bfd30e349786a00046d0661e510
Fike Etki <etki@etki.name>
2014-06-18 22:38:38 +0400

    Updated boot points

--- Yii/boot/testing-console.php
+++ Yii/boot/testing-console.php
@@ -4,3 +4,4 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 require_once __DIR__.'/../vendor/yiisoft/yii/framework/yiit.php';
 $config = require __DIR__.'/../config/testing.php';
 Yii::createWebApplication($config);
+Yii::app()->cache->flush();
