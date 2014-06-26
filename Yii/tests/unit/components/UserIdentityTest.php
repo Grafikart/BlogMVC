6e77bfcc5926823171fc044cc6fc7a85d0a37187
Fike Etki <etki@etki.name>
2014-06-26 15:17:20 +0400

    Added simple UserIdentity test

--- Yii/tests/unit/components/UserIdentityTest.php
+++ Yii/tests/unit/components/UserIdentityTest.php
@@ -2,6 +2,8 @@
 namespace components;
 
 
+use Codeception\Util\Fixtures;
+
 class UserIdentityTest extends \Codeception\TestCase\Test
 {
    /**
@@ -18,9 +20,33 @@ class UserIdentityTest extends \Codeception\TestCase\Test
     }
 
     // tests
-    public function testMe()
+    public function testAuthentication()
     {
+        $username = Fixtures::get('data:users[0]:login');
+        $password = Fixtures::get('data:users[0]:password');
 
+        // setting inexsiting username
+        $identity = new \UserIdentity(md5(mt_rand(0, PHP_INT_MAX)), $password);
+        $this->assertFalse($identity->authenticate());
+        $this->assertSame(
+            $identity->errorCode,
+            \UserIdentity::ERROR_USERNAME_INVALID
+        );
+
+        // setting inexisting password
+        $identity->password = $identity->username;
+        $identity->username = $username;
+        $this->assertFalse($identity->authenticate());
+        $this->assertSame(
+            $identity->errorCode,
+            \UserIdentity::ERROR_PASSWORD_INVALID
+        );
+
+        $identity->password = $password;
+        $this->assertTrue($identity->authenticate());
+        $this->assertSame(
+            $identity->errorCode,
+            \UserIdentity::ERROR_NONE
+        );
     }
-
 }
