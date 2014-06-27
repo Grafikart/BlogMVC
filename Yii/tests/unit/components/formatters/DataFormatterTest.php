4b71b42fa95d77e2b47444c7e0ea0e7052938619
Fike Etki <etki@etki.name>
2014-06-27 07:53:45 +0400

    Updated DataFormatter test

--- Yii/tests/unit/components/formatters/DataFormatterTest.php
+++ Yii/tests/unit/components/formatters/DataFormatterTest.php
@@ -3,16 +3,16 @@
 /**
  * Testing suite for DataFormatter class.
  *
- * @todo more test examples for slug services
  * @todo more coverage, please
  *
- * @author Fike Etki <etki@etki.name>
- * @version 0.1.0
- * @since 0.1.0
+ * @version    Release: 0.1.0
+ * @since      0.1.0
  * @package    BlogMVC
- * @subpackage Yii
+ * @subpackage YiiTests
+ * @author     Fike Etki <etki@etki.name>
  */
-class DataFormatterTest extends \CTestCase {
+class DataFormatterTest extends \Codeception\TestCase\Test
+{
     /**
      * Data provider for {@link testDeslugify()} method
      *
@@ -24,7 +24,11 @@ class DataFormatterTest extends \CTestCase {
         return array(
             array('test-slug', 'test slug'),
             array('space-pilot-3000', 'space pilot 3000'),
-            array('push-the-little--cart------heavy', 'Push The Little Cart — Heavy', true),
+            array(
+                'push-the-little--cart------heavy',
+                'Push The Little Cart — Heavy',
+                true
+            ),
         );
     }
 
@@ -40,7 +44,10 @@ class DataFormatterTest extends \CTestCase {
             array('Push The Little Cart — Heavy', 'push-the-little-cart---heavy',),
             array('    wanna Dance  ?  ', 'wanna-dance',),
             array('Test Pilot 3000', 'test-pilot-3000',),
-            array('Дорогая -- ты сегодня уже рзжни?', 'дорогая---ты-сегодня-уже-рзжни',),
+            array(
+                'Дорогая -- ты сегодня уже рзжни?',
+                'дорогая---ты-сегодня-уже-рзжни',
+            ),
             array('Myötähäpeä', 'myotahapea', true),
             array('space-pilot-3000', 'space-pilot-3000', true),
         );
@@ -49,17 +56,19 @@ class DataFormatterTest extends \CTestCase {
     /**
      * Tests slugification service.
      *
+     * @param string  $text           Text to be slugified.
+     * @param string  $expectedOutput Expected slug.
+     * @param boolean $translit       If true, ASCII-transliteration is applied.
+     *
      * @dataProvider slugifyDataProvider
      *
-     * @param string $text Text to be slugified.
-     * @param string $expectedOutput Expected slug.
-     * @param boolean $translit If true, ASCII-transliteration is applied.
+     * @return void
      * @since 0.1.0
      */
     public function testSlugify($text, $expectedOutput, $translit=false)
     {
         $this->assertSame(
-            Yii::app()->formatter->slugify($text, $translit),
+            \Yii::app()->formatter->slugify($text, $translit),
             $expectedOutput
         );
     }
@@ -67,19 +76,35 @@ class DataFormatterTest extends \CTestCase {
     /**
      * Tests conversion from slug to text.
      *
+     * @param string  $slug           Slug to process,
+     * @param string  $expectedOutput Expected text.
+     * @param boolean $capitalize     If set to true, {@link DataFormatter} will
+     * be told to capitalize every word.
+     *
      * @dataProvider deslugifyDataProvider
      *
-     * @param string $slug Slug to process,
-     * @param string $expectedOutput Expected text.
-     * @param boolean $capitalize If set to true, {@link DataFormatter} will
-     * be told to capitalize every word.
+     * @return void
      * @since 0.1.0
      */
     public function testDeslugify($slug, $expectedOutput, $capitalize=false)
     {
         $this->assertSame(
-            Yii::app()->formatter->deslugify($slug, $capitalize),
+            \Yii::app()->formatter->deslugify($slug, $capitalize),
             $expectedOutput
         );
     }
-} 
+
+    /**
+     * Tests array rendering.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function testArrayRendering()
+    {
+        $path = \Yii::getPathOfAlias('application.config.web').'.php';
+        $dummy = include $path;
+        $dummy2 = 'return '.\Yii::app()->formatter->renderArray($dummy).';';
+        $this->assertEquals($dummy, eval($dummy2));
+    }
+}
