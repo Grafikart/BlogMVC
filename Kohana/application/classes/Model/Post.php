3ebdebfdde795722808a4133791cdc859bbf7725
macdu <dufay.marc@gmail.com>
2014-07-11 16:24:35 +0200

    Ajout des fichiers models de base

---
+++ Kohana/application/classes/Model/Post.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Post extends ORM{
+	
+}
---
+++ Kohana/application/classes/Model/User.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class User extends ORM{
+	
+}
