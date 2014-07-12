48d244cef8b389a13285824b9dbf0ba6a80b09cc
macdu <dufay.marc@gmail.com>
2014-07-12 12:11:37 +0200

    Création de la sidebar

--- Kohana/application/views/blog/sidebar.php
+++ Kohana/application/views/blog/sidebar.php
@@ -1,13 +1,13 @@
 <h4>Categories</h4>
 <div class="list-group">
-	@foreach ($sidebar_categories as $category)
-		{{ HTML::decode(HTML::linkRoute('category', '<span class="badge">'.$category->post_count.'</span> '.e($category->name), array($category->slug), array('class' => 'list-group-item'))) }}
-	@endforeach
+	<?php foreach ($sidebar_categories as $category): ?>
+		<?= html_entity_decode(HTML::anchor('category/' . $category->slug, '<span class="badge">'.$category->post_count.'</span> '.HTML::chars($category->name), array('class' => 'list-group-item'))) ?>
+	<?php endforeach ?>
 </div>
 
 <h4>Last posts</h4>
 <div class="list-group">
-	@foreach ($sidebar_posts as $post)
-		{{ HTML::linkRoute('post', e($post->name), array($post->slug), array('class' => 'list-group-item')) }}
-	@endforeach
+	<?php foreach ($sidebar_posts as $post): ?>
+		<?= HTML::anchor('post/' . $post->slug, HTML::chars($post->name), array('class' => 'list-group-item')) ?>
+	<?php endforeach ?>
 </div>
