73d70f6387916d2d7a7cf72ead5b34cc1a500f04
macdu <dufay.marc@gmail.com>
2014-07-12 12:44:13 +0200

    Mise à jour du fichier routes.php

--- Kohana/application/routes.php
+++ Kohana/application/routes.php
@@ -1,5 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// Je crée une fonction permettant de vérifier que la requête est bien une requête POST
+$postRequest = function($route, $params, $request)
+{
+	if ($request->method() !== HTTP_Request::POST)
+        {
+            return FALSE; // This route only matches POST requests
+        }
+};
+
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
@@ -36,9 +45,53 @@ Route::set('user', 'author/<user>',
 	));
 
 //Voir un post
-Route::set('slug', '<slug>', 
+Route::set('slug', 'post/<slug>', 
 	array( 'slug' => '[0-9a-z\-]+' ))
 	->defaults(array(
 		'controller' => 'posts',
 		'action' => 'show'
 	));
+
+Route::set('comment','comment/<post_slug>' , 
+	array( 'post_slug' => '[0-9a-z\-]+' ))
+	->filter($postRequest)
+	->defaults(array(
+			'controller' => 'post',
+			'action' => 'create_comment'
+		));
+
+Route::set('post_login', 'login')
+	->filter($postRequest)
+	->defaults(array(
+			'controller' => 'guest',
+			'action' => 'post_login'
+		));
+
+//Si ce n'est pas une action POST
+Route::set('get_login', '/login')
+	->defaults(array(
+			'controller' => 'guest',
+			'action' => 'get_login'
+		));
+
+Route::set('admin' , 'admin')
+	->defaults(array(
+			'controller' => 'admin',
+			'action' => 'index'
+		));
+
+Route::set('admin_edit' , 'admin/post/<action>(/<post_slug>)',
+	array('action' => '(create|edit|delete)'))
+	->filter(function($route, $params, $request){
+		//permet de faire un controlleur de type REST
+		if($request->method() == HTTP_Request::POST){
+			$params['action'] = 'post_' . $params['action'];
+		} else {
+			$params['action'] = 'get' . $params['action'];
+		}
+		return $params;
+
+	})
+	->defaults(array(
+			'controller' => 'admin'
+		));
