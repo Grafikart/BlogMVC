f74d87f0759a4d1b7247b6e00fafe50ceca8ea63
Fike Etki <etki@etki.name>
2014-07-22 20:33:41 +0400

    Started API test suite

---
+++ Yii/tests/api/CategoryAjaxSaveCept.php
@@ -0,0 +1,45 @@
+<?php
+/** @type \Codeception\Scenario $scenario */
+$scenario->groups('api', 'ajax');
+
+$I = new ApiGuy($scenario);
+$I->wantTo('Save category via ajax call');
+
+$url = \Yii::app()->createUrl('category/ajaxSave');
+$uniqueName = md5(microtime());
+
+$I->assertNull(\Category::model()->findBySlug($uniqueName));
+$I->sendPOST($url, array(
+    'Category[name]' => $uniqueName,
+    'Category[slug]' => $uniqueName,
+));
+$I->seeResponseCodeIs(200);
+$id = $I->grabDataFromJsonResponse('data.id');
+$slug = $I->grabDataFromJsonResponse('data.slug');
+$I->assertNotNull($category = \Category::model()->findBySlug($uniqueName));
+$I->assertEquals($id, $category->id);
+$I->assertEquals($slug, $category->slug);
+
+$uniqueName = md5(microtime());
+$I->sendPOST($url, array(
+    'Category[id]' => $id,
+    'Category[name]' => $uniqueName,
+    'Category[slug]' => $uniqueName,
+));
+$I->assertEquals($id, $I->grabDataFromJsonResponse('data.id'));
+$I->assertNotNull($category = \Category::model()->findBySlug($uniqueName));
+$I->assertEquals($category->id, $I->grabDataFromJsonResponse('data.id'));
+$I->assertEquals($uniqueName, $I->grabDataFromJsonResponse('data.slug'));
+
+$I->sendPOST($url, array(
+    'Category[name]' => $uniqueName,
+    'Category[slug]' => $uniqueName,
+));
+$I->assertNotEquals($id, $I->grabDataFromJsonResponse('data.id'));
+$I->assertEquals($uniqueName.'-1', $I->grabDataFromJsonResponse('data.slug'));
+
+$I->sendPOST($url, array(
+    'Category[slug]' => $uniqueName,
+));
+$I->assertFalse($I->grabDataFromJsonResponse('success'));
+$I->assertNotEmpty($I->grabDataFromJsonResponse('errors'));
---
+++ Yii/tests/api/_bootstrap.php
@@ -0,0 +1,2 @@
+<?php
+\Yii::app()->fixtureManager->prepare();
