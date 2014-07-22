fd01dd70c41205c23c0fa869652958e6e1dc8888
Fike Etki <etki@etki.name>
2014-07-22 21:42:20 +0400

    SettingsCept

--- Yii/tests/acceptance/SettingsCept.php
+++ Yii/tests/acceptance/SettingsCept.php
@@ -1,42 +1,23 @@
 <?php
+/** @type \Codeception\Scenario $scenario */
+$scenario->groups('admin', 'settings');
+
 $I = new \WebGuy\MemberSteps($scenario);
 $I->wantTo('Play with settings');
-$I->amGoingTo('Ensure cache is handled right and change application options');
+$I->amGoingTo('Tweak themes, languages and names');
 
-$I->login('admin', 'admin');
+$I->autoLogin();
 $I->amOnPage(\OptionsPage::$url);
-$I->setCurrentPage('\OptionsPage');
-$defaultTitle = $I->grabValueFrom(\OptionsPage::$appTitleField);
-$defaultLanguage = $I->grabValueFrom(\OptionsPage::$siteLanguageList);
-$defaultTheme = $I->grabValueFrom(\OptionsPage::$themeList);
-
-$I->currentPage->updateOptions(
+$I->setCookie('useDefaultLanguage', 'true');
+\OptionsPage::of($I)->updateOptions(
     'Horse Tickler\'s Dagger Fan Club',
     'ru',
-    'simple'
+    'ambinight'
 );
 $I->seeCurrentUrlEquals(\OptionsPage::$url);
 $I->seeInTitle('Horse Tickler\'s Dagger Fan Club');
 $I->seeOptionIsSelected(\OptionsPage::$siteLanguageList, 'ru');
-$I->seeOptionIsSelected(\OptionsPage::$themeList, 'simple');
-
-$I->currentPage->updateOptions(
-    $defaultTitle,
-    $defaultLanguage,
-    $defaultTheme
-);
-
-$I->adminLogin();
+$I->seeOptionIsSelected(\OptionsPage::$themeList, 'ambinight');
+$I->see('Настройки', \OptionsPage::$pageHeaderSelector);
 $I->amOnPage(\BlogFeedPage::$url);
-$categories = \FeedPage::of($I)->grabCategories();
-$stats = \ServiceStatusPage::of($I)->grabStats();
-
-$I->writePost('dummy post', 'dummy post', 'dummy-post', $categories[0]['name']);
-
-$I->amOnPage(\ServiceStatusPage::$url);
-$I->see($stats['postsToday'] + 1, \ServiceStatusPage::$postsTodayContainer);
-$I->see($stats['totalPosts'] + 1, \ServiceStatusPage::$totalPostsContainer);
-
-$I->amOnPage(\BlogFeedPage::$url);
-$I->see($categories[0]['amount'] + 1, '.categories .item-1');
-
+$I->see('Категории', \BlogFeedPage::$sidebarSelector);
