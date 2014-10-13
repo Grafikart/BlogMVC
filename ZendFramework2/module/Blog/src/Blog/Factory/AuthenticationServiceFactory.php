e130a9913c13cf9e791b8da2d3fc63ecaf441ac2
Florent <florent.blaison@happn.fr>
2014-10-13 09:37:28 +0200

    [FIX] issue #54

---
+++ ZendFramework2/module/Blog/src/Blog/Factory/AuthenticationServiceFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Blog\Factory;
+
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class AuthenticationServiceFactory implements FactoryInterface {
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return $serviceLocator->get('doctrine.authenticationservice.orm_default');
+    }
+}
