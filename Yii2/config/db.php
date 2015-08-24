fa6703519cbaa1de0432d45676b95f9fdf342926
Cyril Marois <cmarois@ibitux.com>
2015-08-24 12:47:31 +0200

    chmod and set config db

--- Yii2/config/db.php
+++ Yii2/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=blogMvc',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
