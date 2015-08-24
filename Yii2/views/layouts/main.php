202818181d4d903f7cda5ee787ea765f3834130f
Cyril Marois <cmarois@ibitux.com>
2015-08-24 12:56:22 +0200

    remove useless nav and pages

--- Yii2/views/layouts/main.php
+++ Yii2/views/layouts/main.php
@@ -36,9 +36,6 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ?
                 ['label' => 'Login', 'url' => ['/site/login']] :
                 [
