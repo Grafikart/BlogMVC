706088850f9ea57f27c6049009ad84686882644d
madeindjs <madeindjs@gmail.com>
2016-12-22 09:35:20 +0100

    add bootsrap on Post form

--- ZendFramework1/application/forms/Post.php
+++ ZendFramework1/application/forms/Post.php
@@ -12,35 +12,40 @@ class Application_Form_Post extends Zend_Form
 		$this->addElement('text', 'name', array(
 				'label'	=> 'Name of this post',
 				'required' => true,
-				'filters' => array('StringTrim')
+				'filters' => array('StringTrim'),
+				'class' => 'form-control'
 			)
 		);
 
 		$this->addElement('textarea', 'content', array(
 				'label'	=> 'Content of your post',
 				'required' => true,
-				'filters' => array('StringTrim')
+				'filters' => array('StringTrim'),
+				'class' => 'form-control'
 			)
 		);
 
 		$this->addElement('text', 'categoryId', array(
 				'label'	=> 'Id of the category',
 				'required' => true,
-				'filters' => array('Int')
+				'filters' => array('Int'),
+				'class' => 'form-control'
 			)
 		);
 
 		$this->addElement('text', 'userId', array(
 				'label'	=> 'Id of the creator',
 				'required' => true,
-				'filters' => array('Int')
+				'filters' => array('Int'),
+				'class' => 'form-control'
 			)
 		);
 
 
 		$this->addElement('submit', 'submit', array(
 				'ignore' => true,
-				'label' => 'create this post'
+				'label' => 'create this post',
+				'class' => 'form-control'
 			)
 		);
 
